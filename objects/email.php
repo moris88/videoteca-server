6fdc1fccf1d7e1639155132676908acd1fed8b35
Maurizio Tolomeo <37340833+moris88@users.noreply.github.com>
2021-05-20 19:37:23 +0200

    update 4

--- objects/email.php
+++ objects/email.php
@@ -15,7 +15,7 @@ function send_email($data, $elencoFilm){
     $mail = new PHPMailer(true);
     try {
         //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
